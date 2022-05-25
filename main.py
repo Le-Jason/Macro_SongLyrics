22bfab2f1039fb719d3755539c96d81ccbe986d6
Le-Jason <jason.le7131@gmail.com>
2022-05-24 18:02:02 -0600

    added blue fish red fish

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import pyautogui
 import pydirectinput
 import keyboard 
 import os
-import csv
 
-pydirectinput.PAUSE = 0.01
+pydirectinput.PAUSE = 0.001
 
 
+global readLineIdx
+readLineIdx = 0
 
 def forestEvent():
     pydirectinput.press('enter')
     pydirectinput.write('/all ')
     pydirectinput.keyDown('shift')
-    pydirectinput.write('run forest run1')
+    pydirectinput.write('run from the king1')
     pydirectinput.keyUp('shift')
     pydirectinput.press('enter')
 
@@ -32,12 +33,50 @@ def karenEvent():
     pydirectinput.keyUp('shift')
     pydirectinput.press('enter')
 
+def reset():
+    pydirectinput.keyUp('shift')
+
+def readText(content):
+    
+
+    pydirectinput.press('enter')
+    pydirectinput.write('/all ')
+    pydirectinput.keyDown('shift')
+    pydirectinput.write(content)
+    pydirectinput.keyUp('shift')
+    pydirectinput.press('enter')
+
+
+def fixText(content):
+    for i in range(0,len(content)-1):
+        content[i] = content[i].lower()
+        print(content[i])
+
+    return content
+cnt = 0
+fileName = os.path.join("Data","RedFishBlueFish.txt")
+
 while True:
-    if keyboard.is_pressed('ctrl+r'):
-        while(keyboard.is_pressed('ctrl+r')):
-            pass
-        karenEvent()
-    if keyboard.is_pressed('ctrl+t'):
-        while(keyboard.is_pressed('ctrl+t')):
-            pass
-        karenEvent()
+    try:
+        with open(fileName,'r') as f:
+            if(cnt == 0):
+                f_content = f.readlines()
+                f_content = fixText(f_content)
+                print(f_content)
+                cnt = 1
+            if keyboard.is_pressed('0'):
+                while(keyboard.is_pressed('0')):
+                    pass
+                forestEvent()
+            if keyboard.is_pressed('9'):
+                while(keyboard.is_pressed('9')):
+                    pass
+                karenEvent()
+            if keyboard.is_pressed('8'):
+                while(keyboard.is_pressed('8')):
+                    pass
+                readText(f_content[readLineIdx])
+                readLineIdx = readLineIdx + 1
+            reset()
+    except:
+        pass
--- test.py
+++ test.py
@@ -1,33 +1,9 @@
-import pydirectinput
-import keyboard 
+import os
 
-pydirectinput.PAUSE = 0.01
+def readData():
+    fileName = os.path.join("Data","RedFishBlueFish.txt")
+    with open(fileName,'r') as f:
+        f_content = f.readline()
+        print(f_content)
 
-
-
-def forestEvent():
-    pydirectinput.press('enter')
-    pydirectinput.write('/all ')
-    pydirectinput.keyDown('shift')
-    pydirectinput.write('run forest run1')
-    pydirectinput.keyUp('shift')
-    pydirectinput.press('enter')
-
-def karenEvent():
-    pydirectinput.press('enter')
-    pydirectinput.write('/all ')
-    pydirectinput.keyDown('shift')
-    pydirectinput.write('its just a video game1 are you really mad at pixels/')
-
-while True:
-    try:
-        if keyboard.is_pressed('ctrl+r'):
-            while(keyboard.is_pressed('ctrl+r')):
-                pass
-            forestEvent()
-        if keyboard.is_pressed('ctrl+k'):
-            while(keyboard.is_pressed('ctrl+k')):
-                pass
-            karenEvent()
-    except:
-        pass
+readData()
